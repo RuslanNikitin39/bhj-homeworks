42d1d45162b86ab822c14159ab26268d462a10df
RuslanNikitin39 <ruslan-nikitin@mail.ru>
2022-09-17 11:14:27 +0200

    task_2_3

---
+++ element-search/slider/task.js
@@ -0,0 +1,33 @@
+const slides = document.querySelectorAll('.slider__item');
+const prev = document.querySelector('.slider__arrow_prev');
+const next = document.querySelector('.slider__arrow_next');
+
+let numSld = 0;
+
+next.onclick = function() {
+    numSld++;
+    for (let slide of Array.from(slides)) {
+        slide.className = 'slider__item';
+    }
+
+    if (numSld >= slides.length) {
+        numSld = 0;
+    }
+
+    slides[numSld].className = 'slider__item slider__item_active';
+}
+
+prev.onclick = function() {
+    numSld--;
+    
+    for (let slide of Array.from(slides)) {
+        slide.className = 'slider__item';
+    }
+
+    if (numSld < 0) {
+        numSld = slides.length - 1;
+    }
+
+    slides[numSld].className = 'slider__item slider__item_active';
+}
+
