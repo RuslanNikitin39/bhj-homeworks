7597f7e88ce1a22e4ca808b8fc027863e0cfb3e7
RuslanNikitin39 <ruslan-nikitin@mail.ru>
2022-09-18 23:07:55 +0200

    fix task_2_2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,11 +4,13 @@ for (let lnk of Array.from(links)) {
     lnk.onclick = function() {
 
         const parent = lnk.parentElement;
-
-        if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
-            parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
-        } else {
-            parent.querySelector('.menu_sub').className = 'menu menu_sub';
+        
+        if (parent.querySelector('.menu_sub') !== null) {
+            if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
+                parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
+            } else {
+                parent.querySelector('.menu_sub').className = 'menu menu_sub';
+            }
         }
 
         if (lnk.closest('.menu_main')) {
