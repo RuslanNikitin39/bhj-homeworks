15b622785cd76d056879505227ff2928a19e4180
RuslanNikitin39 <ruslan-nikitin@mail.ru>
2022-09-18 22:39:19 +0200

    fix task_2_1

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -5,15 +5,22 @@ const success = document.querySelector('#modal_success');
 
 popUp.className = 'modal modal_active';
 
-btnClose[0].onclick = function(){
-    popUp.className = 'modal';
-};
+for (let btn of btnClose) {
+    btn.onclick = function(){
+        // popUp.className = 'modal';
+        // success.className = 'modal';
+        this.closest('div.modal').className = 'modal'
+    };
+}
+// btnClose[0].onclick = function(){
+//     popUp.className = 'modal';
+// };
 
 btnSuccess.onclick = function(){
     success.className = 'modal modal_active';
 };
 
-btnClose[1].onclick = function(){
-    popUp.className = 'modal';
-    success.className = 'modal';    
-};
+// btnClose[1].onclick = function(){
+//     popUp.className = 'modal';
+//     success.className = 'modal';    
+// };
