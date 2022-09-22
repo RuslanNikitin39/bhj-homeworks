d0277d9de54f8b791a4671ade75e4a13aff0b448
RuslanNikitin39 <ruslan-nikitin@mail.ru>
2022-09-22 11:28:04 +0200

    task_5_1

---
+++ html-forms/chat/task.js
@@ -0,0 +1,70 @@
+const chatVidget = document.querySelector('.chat-widget')
+const messages = document.querySelector('.chat-widget__messages')
+const input = document.getElementById('chat-widget__input')
+
+const container = document.querySelector('.chat-widget__messages-container')
+
+let flg = 1
+
+chatVidget.addEventListener('click', () => {
+    chatVidget.classList.add('chat-widget_active')
+})
+
+const arrMsgs = [
+    'Кто здесь?',
+    'Давай, досвидания!!!',
+    'Приходите завтра.',
+    'Что хотели?',
+    'Ошиблись номером.'
+]
+
+function addZero(i){
+    if (i < 10) {
+        i = "0" + i
+    }
+    return i
+}
+
+function getTime(){
+    const time = new Date()
+    return `${addZero(time.getHours())}:${addZero(time.getMinutes())}`
+}
+
+input.addEventListener('change', (event) => {
+    
+    flg = 0
+
+    if (event.target.value !== "") {
+        
+        messages.innerHTML += `
+        <div class="message message_client">
+        <div class="message__time">${getTime()}</div>
+        <div class="message__text">${event.target.value}</div>
+    </div>
+        `
+        event.target.value = "" 
+        
+        botAnswer(arrMsgs)
+        container.scrollTo(0, container.scrollHeight)
+    }   
+})
+
+function botAnswer(answer) {
+    const index = Math.floor(Math.random() * answer.length)
+
+    return messages.innerHTML += `
+    <div class="message">
+    <div class="message__time">${getTime()}</div>
+    <div class="message__text">${answer[index]}</div>
+    </div>
+    `
+}
+
+tm = setInterval(() => {
+    if(flg){
+        botAnswer(arrMsgs)
+        container.scrollTo(0, container.scrollHeight)
+    } else {
+        flg = 1
+    }
+}, 10000, flg);
